8d7837c42f91e536fedbf07740d03121ce17f9fe
Dimitry Rudenko <3952800@gmail.com>
2020-03-27 20:43:01 +0100

    fix swagger urls (add /api)

--- src/main/java/com/supermarket/backend/config/Swagger.java
+++ src/main/java/com/supermarket/backend/config/Swagger.java
@@ -1,16 +1,27 @@
 package com.supermarket.backend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.ServletContext;
 
 @Configuration
 @EnableSwagger2
 public class Swagger {
+
+    private final ServletContext servletContext;
+
+    @Autowired
+    public Swagger(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
 
     @Bean
     public Docket api() {
@@ -18,7 +29,13 @@ public class Swagger {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .pathProvider(new RelativePathProvider(servletContext) {
+                    @Override
+                    public String getApplicationBasePath() {
+                        return "/api";
+                    }
+                });
     }
 
 }
